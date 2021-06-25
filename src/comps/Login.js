956398928dd69daeaab46f519fa14d6831cf2acc
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-06-26 00:11:52 +0530

    Login and signup done

--- src/comps/Login.js
+++ src/comps/Login.js
@@ -2,66 +2,51 @@ import React,{Component} from 'react';
 import fire from '../firebase/config.js';
 import { useState } from 'react';
 
-class Login extends Component{
-    constructor(props)
-    {
-        super(props);
-        this.login = this.login.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.signup = this.signup.bind(this);
-        this.state={
-            email : "",
-            password : ""
-        }
-    }
-    login(e){
+const Login=()=>{
+    const[email,setEmail]=useState(null);
+    const[password,setPassword]=useState(null);
+
+    const login=(e)=>{
         e.preventDefault();
-        fire.auth().signInWithEmailAndPassword(this.state.email,this.state.password).then((u)=>{
+        fire.auth().signInWithEmailAndPassword(email,password).then((u)=>{
             console.log(u)
         }).catch((err)=>{
             console.log(err);
         })
     }
-    signup(e){
+    const signup=(e)=>{
         e.preventDefault();
-        fire.auth().createUserWithEmailAndPassword(this.state.email,this.state.password).then((u)=>{
+        fire.auth().createUserWithEmailAndPassword(email,password).then((u)=>{
             console.log(u)
         }).catch((err)=>{
             console.log(err);
         })
     }
-    handleChange(e){
-        this.setState({
-            [e.target.name] : e.target.value
-        })
-    }
-    render()
-    {
-        return(
-            <div>
-                <form>
-                    <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="enter email address"
-                    onChange={this.handleChange}
-                    value={this.state.email}
-                    />
-                    <input
-                    name="password"
-                    type= "password"
-                    onChange={this.handleChange}
-                    id="password"
-                    placeholder="enter password"
-                    value={this.state.password}
-                    />
-                    <button onClick={this.login}>Login</button>
-                    <button onClick={this.signup}>Signup</button>
-                </form>
-    
-            </div>
-        )
-    }
-    }
+    return(
+        <div>
+            <form>
+                <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="enter email address"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                />
+                <input
+                name="password"
+                type= "password"
+                id="password"
+                placeholder="enter password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                />
+                <button onClick={login}>Login</button>
+                <button onClick={signup}>Signup</button>
+            </form>
+
+        </div>
+    )
+}
+
 export default Login;
