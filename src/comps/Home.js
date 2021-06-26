e3d32e54999f30ced0b092a8cdcf0a660e798267
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-06-26 23:43:45 +0530

    Home page file added

---
+++ src/comps/Home.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+const Home = () => {
+    return ( 
+        <div>
+            <h1>This is Home page</h1>
+        </div>
+     );
+}
+ 
+export default Home;
