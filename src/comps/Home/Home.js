3d6728e56af9d198eb5302521206a7587b352033
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-07-03 19:55:01 +0530

    Css added for home

--- src/comps/Home/Home.js
+++ src/comps/Home/Home.js
@@ -1,18 +1,18 @@
 import React,{useContext} from 'react';
 import {Link} from "react-router-dom";
 import { AuthContext } from '../Auth/Auth';
+import './Home.css';
 
 const Home = () => {
     const{currentUser}=useContext(AuthContext)
     return ( 
         <div>
-
-            <h1>Home Page</h1>
+            <h1>Hola Users!</h1>
+            <h2>This is site where you can upload your collection of photos and view them in a grid-like structure</h2>
             {currentUser?
                 (<p>You are logged in-<Link to="/dashboard">View Your Dashboard</Link></p>):
                 (<p>You can Login or Signup</p>)
-            }
-                
+            }                
         </div>
      );
 }
