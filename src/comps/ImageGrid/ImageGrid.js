a7e776541a011496e5ee12535222229a006eac51
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-07-05 22:26:47 +0530

    Changes in title

--- src/comps/ImageGrid/ImageGrid.js
+++ src/comps/ImageGrid/ImageGrid.js
@@ -4,6 +4,7 @@ import {motion} from 'framer-motion';
 import './ImageGrid.css'
 
 const ImageGrid= ({setSelectedImage}) => {
+    /* const id=us */
     const {docs}=useFirestore('images');
 
     return (
