4baefbc52171312374d7a1bedc820717afc1ee01
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-07-09 10:53:11 +0530

    Async function added for Signup

--- src/comps/Signup/Signup.js
+++ src/comps/Signup/Signup.js
@@ -5,21 +5,25 @@ import '../Login/Login.css'
 
 
 const Signup= () => {
-    /* const[email,setEmail]=useState(null);
-    const[password,setPassword]=useState(null); */
 
     const[currentUser,setCurrentUser]=useState(null);
     const handleSubmit=(e)=>{
         e.preventDefault();
         const{email,password}=e.target.elements; 
 
-        try{
+/*         try{
             fire.auth().createUserWithEmailAndPassword(email.value,password.value);
             setCurrentUser(true);
         }
         catch(error){
             alert(error);
-        }
+        } */
+        fire.auth().createUserWithEmailAndPassword(email.value,password.value).then((u)=>{
+            setCurrentUser(true);
+            console.log(u);
+        }).catch((err)=>{
+            alert(err);
+        })
     }
 
     if(currentUser){
