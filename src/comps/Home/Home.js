c24c13a177d68e0e7260946305b6d0a7c9dcea18
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-07-09 23:40:36 +0530

    Home component - Link to dashboard changed

--- src/comps/Home/Home.js
+++ src/comps/Home/Home.js
@@ -1,23 +1,34 @@
-import React,{useContext} from 'react';
+import React,{useContext,useState} from 'react';
 import {Link} from "react-router-dom";
 import { AuthContext } from '../Auth/Auth';
 import './Home.css';
 
 const Home = () => {
     const{currentUser}=useContext(AuthContext)
+    /* const[userId,setUserId]=useState(null); */
+
+    var usr;
+    if(currentUser){
+        var usr=currentUser.uid
+    }
+
+   
     return ( 
         <div>
             <h1>Hola Users!</h1>
             <h2>This is site where you can upload your collection of photos and view them in a grid-like structure</h2>
             {currentUser?
-                (<p>You are logged in-<Link to="/dashboard">View Your Dashboard</Link></p>):
+                (
+                <p>You are logged in-<Link to={`/dashboard/${usr}`}>View Your Dashboard</Link></p>
+               
+                ):
                 (
                 <div>
                     <p>You can Login or Signup from our navbar</p>
-                    <h4>Take a look at the grid :)</h4>
                 </div>
                 )
-            }                
+            }
+            <h4>Take a look at the grid :)</h4>                
         </div>
      );
 }
