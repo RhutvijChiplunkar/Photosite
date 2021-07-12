ed174fd3f8bdbad54000baac043c805e59a1ca48
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-07-13 00:06:49 +0530

    minor changes

--- src/comps/Title/Title.js
+++ src/comps/Title/Title.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Title.css';
+
 const Title = () => {
 
   return (
     <div className="title">
       <h2>My Photos</h2>
-      <p>My photo collection from various places!</p>
     </div>
   )
 }
