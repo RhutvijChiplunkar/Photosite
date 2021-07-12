bfae662544f7fab7d7a4c0061121407db2f67c79
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-07-13 00:07:42 +0530

    Dashboard for individual user created

--- src/comps/Dashboard/Dashboard.js
+++ src/comps/Dashboard/Dashboard.js
@@ -4,8 +4,14 @@ import './Dashboard.css';
 import {Redirect } from 'react-router-dom';
 import {AuthContext} from '../Auth/Auth';
 import UploadForm from '../UploadForm/UploadForm';
+import TotalPosts from '../TotalPosts/TotalPosts.js';
+import ImageGrid from '../ImageGrid/ImageGrid.js';
+import Modal from '../Modal/Modal.js';
+import NotFound from '../NotFound/NotFound.js';
 
 const Dashboard = () => {
+    //this is used to enlarge an image when clicked
+    const[selectedImage,setSelectedImage]=useState(null);
     const{currentUser}=useContext(AuthContext);
 
     if(!currentUser){
@@ -16,6 +22,9 @@ const Dashboard = () => {
             <h1>Welcome to photosite</h1>
             <p>This is your dashboard. You are logged in successfully</p>
             <UploadForm/>
+            <TotalPosts/>
+            <ImageGrid setSelectedImage={setSelectedImage}/>
+            {selectedImage && <Modal selectedImage={selectedImage} setSelectedImage={setSelectedImage}/>}
             <button onClick={()=>fire.auth().signOut()}>Sign Out</button>
         </div>
     );
