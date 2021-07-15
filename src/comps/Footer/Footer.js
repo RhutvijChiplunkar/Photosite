e454b4390497a9369b9e84376c30d98d19de433d
RhutvijChiplunkar <rhutvij.chiplunkar@gmail.com>
2021-07-15 22:57:07 +0530

    Footer added

---
+++ src/comps/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+const Footer = () => {
+    return ( 
+      <div class="footer">
+        <h3>&copy; Rhutvij Chiplunkar</h3>      
+      </div>
+     );
+}
+ 
+export default Footer;
