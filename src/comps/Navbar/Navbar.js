640d4e22f8b722c39844658fe42ddf3308e345f6
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2021-09-14 00:25:49 +0530

    Changes made in navbar

--- src/comps/Navbar/Navbar.js
+++ src/comps/Navbar/Navbar.js
@@ -5,21 +5,15 @@ import { Navbar, Nav, Container, Button } from "react-bootstrap";
 
 const Navb = () => {
   return (
-    /*     <div className="nav-bar">
-          <Link className="main" to="/">Photosite</Link>
-          <Link to="/login"><button>Login</button></Link>
-          <Link to="/signup"><button>Sign Up</button></Link>
-        </div> */
-
     <div className="navbar-all">
-      <Navbar id="a" fixed="top" collapseOnSelect expand="lg" /* bg="dark" variant="dark" */ className="py-0" >
+      <Navbar id="a" fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark" className="py-0" >
         <Container fluid>
-          <Navbar.Brand> <Link className="main" to="/">Photosite</Link></Navbar.Brand>
-          <Nav className="mb-auto">
+          <Navbar.Brand> <Link to="/" className="title">Photosite</Link></Navbar.Brand>
+          <Nav >
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav.Link href="/login" ><Button className="ms-0 btn btn-warning">Login</Button></Nav.Link>
-            <Nav.Link href="/signup" ><Button className="ms-0 btn btn-warning ">Signup</Button></Nav.Link>
+            <Nav.Link href="/login" ><Button className="btn">Login</Button></Nav.Link>
+            <Nav.Link href="/signup" ><Button className="btn">Signup</Button></Nav.Link>
           </Navbar.Collapse>
           </Nav>
         </Container>
