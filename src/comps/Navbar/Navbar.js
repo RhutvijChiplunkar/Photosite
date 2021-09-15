4c7d3a0e6dbd655015248a77d6701a4d0144e12a
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2021-09-15 19:24:18 +0530

    Proper float for buttons done for hamburger icon

--- src/comps/Navbar/Navbar.js
+++ src/comps/Navbar/Navbar.js
@@ -6,19 +6,19 @@ import { Navbar, Nav, Container, Button } from "react-bootstrap";
 const Navb = () => {
   return (
     <div className="navbar-all">
-      <Navbar id="a" fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark" className="py-0" >
+      <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark" className="py-0" >
         <Container fluid>
           <Navbar.Brand> <Link to="/" className="title">Photosite</Link></Navbar.Brand>
-          <Nav >
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+            </Nav>
             <Nav.Link href="/login" ><Button className="btn">Login</Button></Nav.Link>
             <Nav.Link href="/signup" ><Button className="btn">Signup</Button></Nav.Link>
           </Navbar.Collapse>
-          </Nav>
         </Container>
       </Navbar>
-    </div>
+    </div >
   );
 }
 
