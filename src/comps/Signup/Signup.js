8e7e01160c37a351bc39ea7fd1fe85c46edf8fd2
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2021-09-16 15:36:55 +0530

    Media queries added for responsive design

--- src/comps/Signup/Signup.js
+++ src/comps/Signup/Signup.js
@@ -1,64 +1,64 @@
-import React,{useState} from 'react';
-import fire from '../../firebase/config.js';
+import React, { useState } from 'react';
+import { Button, Form } from "react-bootstrap";
 import { Link } from 'react-router-dom';
-import { Form, Button } from "react-bootstrap";
-import '../Signup/Signup.css'
+import fire from '../../firebase/config.js';
+import '../Signup/Signup.css';
 
 
-const Signup= () => {
+const Signup = () => {
 
-    const[currentUser,setCurrentUser]=useState(null);
-    const[userId,setUserId]=useState(null);
+    const [currentUser, setCurrentUser] = useState(null);
+    const [userId, setUserId] = useState(null);
 
-    const handleSubmit=(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        const{email,password}=e.target.elements; 
+        const { email, password } = e.target.elements;
 
-        fire.auth().createUserWithEmailAndPassword(email.value,password.value).then((u)=>{
+        fire.auth().createUserWithEmailAndPassword(email.value, password.value).then((u) => {
             setCurrentUser(true);
             setUserId(u.user.uid);
             console.log(u.user.uid);
-        }).catch((err)=>{
+        }).catch((err) => {
             alert(err);
         })
     }
 
-    if(currentUser){
+    if (currentUser) {
         return (
             <div className="signup-conf">
-            <h1>User successfully created</h1>
-            <h1><Link  to={`/dashboard/${userId}`}>My dashboard</Link></h1>
+                <h1>User successfully created</h1>
+                <h1><Link to={`/dashboard/${userId}`}>My dashboard</Link></h1>
             </div>
-        ); 
+        );
     }
 
-    return (  
+    return (
         <div className="signup-forms">
-        <Form onSubmit={handleSubmit}>
-            <h2>Signup</h2>
-            <hr/>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" name="email"/>
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+            <Form onSubmit={handleSubmit}>
+                <h2>Signup</h2>
+                <hr />
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" name="email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" name="password" placeholder="Password" />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Password" />
+                </Form.Group>
 
-            <a href='/login'>Already have an account? Login</a><br/><br/>
+                <a href='/login'>Already have an account? Login</a><br /><br />
 
-            <Button variant="primary" type="submit">
-                Signup
-            </Button>
+                <Button variant="primary" type="submit">
+                    Signup
+                </Button>
 
-        </Form>
-    </div>
+            </Form>
+        </div>
     );
 }
- 
+
 export default Signup;
