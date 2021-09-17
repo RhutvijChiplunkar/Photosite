3c6f490fe476732ea09e71ab61154383c0ef7a8c
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2021-09-17 18:14:46 +0530

    Classnames sorted properly

--- src/comps/UploadForm/UploadForm.js
+++ src/comps/UploadForm/UploadForm.js
@@ -4,44 +4,46 @@ import './UploadForm.css';
 
 const UploadForm = () => {
 
-    const[file,setFile]=useState(null);
-    const[error,setError]=useState(null);
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
 
     /*valid types allowed */
-    const types=['image/jpeg','image/png','image/jpg']
+    const types = ['image/jpeg', 'image/png', 'image/jpg']
 
-    const changeHandler=(e)=>{
-        let selected=e.target.files[0];
+    const changeHandler = (e) => {
+        let selected = e.target.files[0];
         /* onsole.log(selected); */
 
         //check if selected is allowed
-        if(selected && types.includes(selected.type)){
+        if (selected && types.includes(selected.type)) {
             setFile(selected);
             setError('');
         }
         //wrong selection
-        else{
+        else {
             setFile(null);
             setError('Please select png or jpeg file');
         }
     }
-    return ( 
-        <form>
-            <h4 className="abc">Add an image</h4>
-            <label className="label-upload">
-            <input type="file" onChange={changeHandler}/>
-            <span className="myDIV">+</span>
-            </label>
-            <div className="output">
-                {/* if error exist in file upload */}
-                {error && <div className="error">{error}</div>}
-                {/* if uploaded correctly */}
-                {file && <div>{file.name} is uploading right now!!</div>}
-
-                {file && <ProgressBar file={file} setFile={setFile}/>}
-            </div>
-        </form>
-     );
+    return (
+        <div className="upload-form">
+            <form>
+                <h4>Add an image</h4>
+                <label>
+                    <span>+</span>
+                    <input type="file" onChange={changeHandler}/>
+                </label>
+                <div className="output">
+                    {/* if error exist in file upload */}
+                    {error && <div className="error">{error}</div>}
+                    {/* if uploaded correctly */}
+                    {file && <div>{file.name} is uploading right now!!</div>}
+                    {/* show progress bar if file is correct */}
+                    {file && <ProgressBar file={file} setFile={setFile} />}
+                </div>
+            </form>
+        </div>
+    );
 }
- 
+
 export default UploadForm;
