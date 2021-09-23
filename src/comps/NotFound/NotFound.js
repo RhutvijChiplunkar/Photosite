b0fd90baaad448c371bf02ede56585b87204f05e
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2021-09-23 17:38:21 +0530

    Not found properly modified

--- src/comps/NotFound/NotFound.js
+++ src/comps/NotFound/NotFound.js
@@ -1,14 +1,15 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import React from 'react';
 import './NotFound.css';
-const NotFound= () => {
-    return (  
-        <div className="not-found">
-        <h2>Sorry</h2>
-        <p>That page cannot be found</p>     
-        <p><Link to="/">Back to the homepage...</Link></p>
-      </div>
-    );
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>Error : 404 page not found</h2>
+      <h3>Sorry, the page you're looking for cannot be accessed !</h3>
+      <Link to="/">Back to the homepage...</Link>
+    </div>
+  );
 }
- 
+
 export default NotFound;
