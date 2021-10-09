a61edba33b0277aa5c69a1742e4ee412e8e39a7b
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2021-10-09 23:45:27 +0530

    Delete functionality added to the project

--- src/comps/ImageGrid/ImageGrid.js
+++ src/comps/ImageGrid/ImageGrid.js
@@ -1,42 +1,50 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import useFirestore from '../../hooks/useFirestore';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 import { AuthContext } from '../Auth/Auth';
 import './ImageGrid.css'
+import { projectFirestore } from '../../firebase/config';
+const ImageGrid = ({ setSelectedImage }) => {
+    const { currentUser } = useContext(AuthContext)
 
-const ImageGrid= ({setSelectedImage}) => {
-    const{currentUser}=useContext(AuthContext)
- 
     var userId;
-    if(currentUser){
-        userId=currentUser.uid;
+    if (currentUser) {
+        userId = currentUser.uid;
     }
-    else{
-        userId='images';
+    else {
+        userId = 'images';
     }
     //const {docs}=useFirestore('images');
-    const {docs}=useFirestore(`${userId}`);
+    const { docs } = useFirestore(`${userId}`);
 
     return (
 
         <div className="img-grid">
-        { docs && docs.map(doc=>(
-            //motion.div when we want to use motion in div
-            <motion.div className="img-wrap" key={doc.id}
-            layout
-            whileHover={{scale:1.15}}
-            //click event added in div tag
-                onClick={()=>setSelectedImage(doc.url)}> 
-                    <motion.img src={doc.url} alt="uploaded pic" 
-                    initial={{opacity:0}}
-                    animate={{opacity:1}}
-                    transition={{delay:1}}
-                    />
-            </motion.div>
-        ))}
+            {docs && docs.map(doc => (
+                <div>
+                    <div>
+                        <button className="cross" onClick={(e)=>{ projectFirestore.collection(`${userId}`).doc(doc.id).delete();}}>x</button>
+                    </div>
+                    {/* //motion.div when we want to use motion in div */}
+                    <motion.div className="img-wrap" key={doc.id}
+                        layout
+                        whileHover={{ scale: 1.15 }}
+                        //click event added in div tag
+                        onClick={() => setSelectedImage(doc.url)}>
+
+                        <motion.img src={doc.url} alt="uploaded pic"
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 1 }}
+                        />
+                    </motion.div>
+
+                </div>
+            ))}
+
         </div>
 
     );
 }
- 
+
 export default ImageGrid;
