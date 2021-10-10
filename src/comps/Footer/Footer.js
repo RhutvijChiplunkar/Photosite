c8cc370a40a94260a0ed2e61fdd1d846b345f851
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2021-10-10 12:05:28 +0530

    Github and Linkedin icons added properly

--- src/comps/Footer/Footer.js
+++ src/comps/Footer/Footer.js
@@ -2,11 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import './Footer.css';
 const Footer = () => {
-    return ( 
-      <div className="footer">
-        <h6>Developed by <a href="https://github.com/RhutvijChiplunkar" target="_blank" rel="noopener noreferrer"><i className="dev">Rhutvij Chiplunkar</i></a></h6>
+  return (
+    <div className="footer">
+      <div>
+        <h4>Developed by Rhutvij Chiplunkar</h4>
+        <a href="www.linkedin.com/in/rhutvij-chiplunkar" target="_blank" rel="noopener noreferrer" className="social-icon"> <i class="fa fa-linkedin"></i></a>
+        <a href="https://github.com/RhutvijChiplunkar" target="_blank" rel="noopener noreferrer" className="social-icon"> <i class="fa fa-github"></i></a>
       </div>
-     );
+    </div>
+  );
 }
- 
+
 export default Footer;
