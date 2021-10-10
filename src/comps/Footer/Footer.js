754bee940d9de63e99bc10c511e87b610ea37090
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2021-10-10 12:41:59 +0530

    Links properly edited

--- src/comps/Footer/Footer.js
+++ src/comps/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="footer">
       <div>
         <h4>Developed by Rhutvij Chiplunkar</h4>
-        <a href="https://www.linkedin.com/in/rhutvij-chiplunkar" target="_blank" rel="noopener noreferrer" className="social-icon"> <i class="fa fa-linkedin"></i></a>
+        <a href="https://linkedin.com/in/rhutvij-chiplunkar" target="_blank" rel="noopener noreferrer" className="social-icon"> <i class="fa fa-linkedin"></i></a>
         <a href="https://github.com/RhutvijChiplunkar" target="_blank" rel="noopener noreferrer" className="social-icon"> <i class="fa fa-github"></i></a>
       </div>
     </div>
